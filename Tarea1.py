c179eb34e957cd394ce83dc78c754a2136eb3969
Abisai2423 <abisaijsue24@gmail.com>
2024-10-07 11:53:25 -0600

    Create Tarea1.py

---
+++ Tarea1.py
@@ -0,0 +1,17 @@
+Realiza un progroma que pida 3 calificaciones y calcule el promedio
+
+
+
+A=int(input ("digite un numero: "))
+
+B=int(input ("digite un numero: "))
+
+C=int(input ("digite un numero: "))
+
+
+
+prom= (x+y+z) / 3
+
+
+
+print (prom
