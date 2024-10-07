198b6b78ac0bf13a712e84aead177968596adf1e
Abisai2423 <abisaijsue24@gmail.com>
2024-10-07 11:59:51 -0600

    Create Tarea2.py

---
+++ Tarea2.py
@@ -0,0 +1,31 @@
+#reaizar el programa de las horas extras
+
+
+
+Horas= int(input ("digite un numero: "))
+
+
+
+if Horas>8:
+
+    pagojornada= 8*200
+
+    horaextra=Horas-8
+
+    pagoextra=horasextra*350
+
+    pagototal=pagohoras + pagoextra
+
+    print(pagoto)
+
+else:
+
+    pagojornada= Horas * 200
+
+    pagototal=pagojornada
+
+    print (pagototal)
+
+
+
+print ("fin del programa")
